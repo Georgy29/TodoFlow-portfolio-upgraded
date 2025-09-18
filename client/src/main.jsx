8afe4bad61346314ea0ea0ac04afed5d5cb2b0cf
Georgy Blokhin <georgy.blokhin@students.opit.com>
2025-09-18 06:37:35 +0200

    add routes (#3)
    * api: auth + JWT-protected todos
    
    * Updated frontend
    
    * added routes + main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
+function LoginPage() { return <h1>Login Page</h1> }
+function RegisterPage() { return <h1>Register Page</h1> }
+function TodosPage() { return <h1>Todos Page</h1> }
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Navigate to="/todos" replace />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/todos" element={<TodosPage />} />
+      <Route path="*" element={<Navigate to="/todos" replace />} />
+    </Routes>
+  </BrowserRouter>
 )
