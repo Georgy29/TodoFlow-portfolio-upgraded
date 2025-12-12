cf8e78ff74f0c757ea8f2513bf0ca385c70da847
Georgy29 <georgy.blokhin@students.opit.com>
2025-12-12 05:52:38 +0100

    Tooling setup and remove unused App

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,18 +1,15 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import importPlugin from 'eslint-plugin-import'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    files: ['src/**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -22,13 +19,30 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      import: importPlugin,
+    },
     rules: {
+      // Base JS recommended rules
+      ...js.configs.recommended.rules,
+
       // React best practices
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react/react-in-jsx-scope': 'off', // not needed in React 17+
+      'react/jsx-uses-react': 'off',
+      'react/jsx-uses-vars': 'warn',
+      'react/jsx-key': 'warn',
+
+      // Hooks rules
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
 
-      // Common mistakes
+      // React Refresh / Vite HMR safety
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+
+      // Common mistakes / cleanups
       'no-unused-vars': [
         'error',
         {
@@ -46,6 +60,15 @@ export default defineConfig([
       'no-var': 'error',
       'object-shorthand': 'warn',
       'prefer-arrow-callback': 'warn',
+
+      // Import ordering
+      'import/order': [
+        'warn',
+        {
+          groups: [['builtin', 'external', 'internal']],
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
     },
   },
 ])
