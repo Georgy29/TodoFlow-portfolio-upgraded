d190e3c05e9dbf0a1403e0d3de5014c607b0470d
George2900 <george.blokhin@outlook.com>
2025-12-26 07:18:30 +0100

    Vitest setup

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,                   // важно в Codespaces
-    port: 5173,                   // можно явно указать
+    host: true,
+    port: 5173,
     proxy: {
       '/api': 'http://localhost:5000'
     }
+  },
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.js'
   }
 })
